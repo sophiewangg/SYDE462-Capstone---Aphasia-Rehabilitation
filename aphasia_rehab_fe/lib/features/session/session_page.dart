0f3cdea951cc87bdacd185ea4c1bcc4d20d649b7
Kelly Pham <phamkelly17@gmail.com>
2026-02-19 20:40:26 -0500

    revert sandbox page to how it was before

--- aphasia_rehab_fe/lib/features/session/session_page.dart
+++ aphasia_rehab_fe/lib/features/session/session_page.dart
@@ -4,10 +4,10 @@ import 'package:permission_handler/permission_handler.dart';
 import '../../models/cue_model.dart';
 import '../../services/transcription_service.dart';
 import '../../services/cue_service.dart';
+import 'widgets/microphone_button.dart';
 import 'widgets/cue_modal.dart';
 import 'widgets/transcription_display.dart';
 import 'scenario_sim.dart';
-import 'package:aphasia_rehab_fe/models/microphone_state.dart';
 
 class SessionPage extends StatefulWidget {
   const SessionPage({super.key, required this.title});
@@ -25,110 +25,6 @@ class _SessionPageState extends State<SessionPage> {
   String _transcription = "";
   String _goal = "Ask for a utensil.";
   bool _isRecording = false;
-  bool _hintButtonPressed = false;
-  String? _likelyWord;
-  List<String> prompts = [
-    "Hello! How are you doing?",
-    "Would you like something to drink?",
-    "What would you like to order?",
-    "Here is your food. Enjoy your meal!",
-    "Can I get you anything else?",
-    "Thank you! Have a great day!",
-  ];
-  int _currentPromptIndex = 0;
-  final ValueNotifier<MicrophoneState> _currentMicrophoneState = ValueNotifier(
-    MicrophoneState.idle,
-  );
-
-  final ValueNotifier<MicrophoneState> _currentMicrophoneStateModal =
-      ValueNotifier(MicrophoneState.idle);
-
-  final ValueNotifier<bool> _cueCompleteNotifier = ValueNotifier(false);
-
-  final ValueNotifier<String?> _cueResultStringNotifier = ValueNotifier(null);
-
-  final ValueNotifier<int> _cueNumberNotifier = ValueNotifier(0);
-
-  bool _modalIsWordFinding = true;
-
-  void updateCueNumber({bool reset = false}) {
-    if (reset) {
-      _cueNumberNotifier.value = 0;
-    } else {
-      _cueNumberNotifier.value = _cueNumberNotifier.value + 1;
-    }
-  }
-
-  void resetCueComplete() {
-    _cueCompleteNotifier.value = false;
-  }
-
-  void resetCueResultString() {
-    _cueResultStringNotifier.value = null;
-  }
-
-  void toggleHintButton() {
-    setState(() {
-      _hintButtonPressed = !_hintButtonPressed;
-    });
-  }
-
-  void updateCurrentMicrophoneState() {
-    if (_currentMicrophoneState.value == MicrophoneState.userSpeaking) {
-      _currentMicrophoneState.value = MicrophoneState.processing;
-
-      Timer(const Duration(seconds: 2), () {
-        if (mounted) {
-          _currentPromptIndex = (_currentPromptIndex + 1) % prompts.length;
-          _currentMicrophoneState.value =
-              MicrophoneState.idle; // This will now auto-update the modal!
-        }
-      });
-    } else if (_currentMicrophoneState.value == MicrophoneState.idle) {
-      _currentMicrophoneState.value = MicrophoneState.userSpeaking;
-    }
-  }
-
-  void updateCurrentMicrophoneStateModal() {
-    if (_currentMicrophoneStateModal.value == MicrophoneState.userSpeaking) {
-      _currentMicrophoneStateModal.value = MicrophoneState.processing;
-      if (_modalIsWordFinding) {
-        processSpeechWordFinding(_likelyWord ?? "");
-      } else {
-        processSpeechUnderstanding();
-      }
-    } else if (_currentMicrophoneStateModal.value == MicrophoneState.idle) {
-      _currentMicrophoneStateModal.value = MicrophoneState.userSpeaking;
-    }
-  }
-
-  void processSpeechWordFinding(String targetWord) async {
-    print("Processing speech. Latest transcript: $_transcription");
-
-    String cleanTranscript = _transcription.toLowerCase().trim();
-    String cleanTarget = targetWord.toLowerCase().trim();
-
-    if (cleanTranscript.contains(cleanTarget)) {
-      print("Success! Word detected.");
-      _cueCompleteNotifier.value = true;
-      _currentMicrophoneStateModal.value = MicrophoneState.idle;
-      _cueResultStringNotifier.value =
-          "Correct! The word is ${targetWord.toUpperCase()}";
-    } else {
-      print("Word not matched yet.");
-      // Revert to idle so they can try again
-      _cueResultStringNotifier.value = "Not quite. Here's another hint:";
-      updateCueNumber();
-      _currentMicrophoneStateModal.value = MicrophoneState.idle;
-    }
-  }
-
-  void processSpeechUnderstanding() {
-    print("Sucess! User indicated they didn't understand.");
-    _cueCompleteNotifier.value = true;
-    _currentMicrophoneStateModal.value = MicrophoneState.idle;
-    _cueResultStringNotifier.value = "Return to exercise.";
-  }
 
   @override
   void initState() {
@@ -140,12 +36,9 @@ class _SessionPageState extends State<SessionPage> {
       setState(() {
         _transcription = result.text;
       });
+
       if (result.isEndOfTurn && _isRecording) {
         _stopRecording(); // Explicitly stop instead of toggling
-        bool isModalOpen = ModalRoute.of(context)?.isCurrent == false;
-        isModalOpen
-            ? updateCurrentMicrophoneStateModal()
-            : updateCurrentMicrophoneState(); // Update the prompt state when the turn ends
       }
     });
   }
@@ -188,64 +81,26 @@ class _SessionPageState extends State<SessionPage> {
     }
   }
 
-  void _handleHintPressed(bool isWordFinding) async {
-    setState(() {
-      _modalIsWordFinding = isWordFinding;
-      _hintButtonPressed = false;
-    });
+  void _handleHintPressed() {
+    // 1. Kick off the request (don't 'await' it here)
+    final cueFuture = _cueService.getCues(_transcription, _goal);
 
-    if (isWordFinding) {
-      // 1. Kick off the request (don't 'await' it here)
-      final cueFuture = _cueService.getCues(_transcription, _goal);
-      toggleHintButton();
-      setState(() {
-        _currentMicrophoneState.value = MicrophoneState.idle;
-      });
-      _stopRecording();
-      // 2. Open the modal immediately
-
-      _showModal(cueFuture);
-
-      // Separately, wait for the future to finish to save the word locally
-      final fetchedCue = await cueFuture;
-
-      if (fetchedCue != null) {
-        setState(() {
-          _likelyWord = fetchedCue.likelyWord;
-        });
-      }
-    } else {
-      // 1. Create a Future that is already finished with null data
-      final noCueFuture = Future<Cue?>.value(null);
-
-      // 2. Open the modal immediately
-      _showModal(noCueFuture);
-    }
+    // 2. Open the modal immediately
+    _showModal(cueFuture);
   }
 
   void _showModal(Future<Cue?> fetchedCue) {
-    _cueCompleteNotifier.value = false;
-    _currentMicrophoneStateModal.value = MicrophoneState.idle;
-
     showModalBottomSheet(
       context: context,
       isScrollControlled: true,
       useSafeArea: true,
-      backgroundColor: Colors.transparent,
-      elevation: 0,
+      constraints: BoxConstraints(maxWidth: MediaQuery.of(context).size.width),
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+      ),
       builder: (BuildContext context) {
-        // AnimatedBuilder is the correct widget for Listenable.merge
-        return AnimatedBuilder(
-          animation: Listenable.merge([
-            _currentMicrophoneStateModal,
-            _cueCompleteNotifier,
-            _cueResultStringNotifier,
-            _cueNumberNotifier,
-          ]),
-          builder: (context, _) {
-            return CueModal(cueFuture: fetchedCue);
-          },
-        );
+        // Pass the service and the current transcription string
+        return CueModal(cueFuture: fetchedCue);
       },
     );
   }
@@ -274,6 +129,20 @@ class _SessionPageState extends State<SessionPage> {
                 );
               },
               child: const Text('Go to scenario'),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: MicrophoneButton(
+                isRecording: _isRecording,
+                onPressed: _handleMicToggle,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 50),
+              child: ElevatedButton(
+                onPressed: _handleHintPressed,
+                child: const Text('I need a hint!'),
+              ),
             ),
           ],
         ),
